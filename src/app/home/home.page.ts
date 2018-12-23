39108475c610288e983f72745d6a5d9049e02197
Samarth Agarwal <samarthagarwal@live.com>
2018-12-23 23:27:50 +0530

    2.5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(private loadingCtrl: LoadingController) {
+
+  }
+
+  async showLoading() {
+    let loading = await this.loadingCtrl.create({
+      message: "Loading...",
+      duration: 5000,
+      showBackdrop: true,
+      spinner: "lines-small"
+    });
+
+    loading.present();
+  }
 }
